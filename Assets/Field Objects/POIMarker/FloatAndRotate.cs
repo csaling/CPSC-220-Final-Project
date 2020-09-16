c604415dae14bc6c24a1e13be277b965fbf4d4df
Cade Saling <csaling@muskingum.edu>
2020-09-16 09:46:18 -0400

    Created Location Based Game / Added POI Marker

---
+++ Assets/Field Objects/POIMarker/FloatAndRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatAndRotate : MonoBehaviour {
+
+	[SerializeField] private float rotateSpeed = 50;
+	[SerializeField] private float floatAmplitude = 1f;
+	[SerializeField] private float floatFrequency = 0.75f;
+
+	private Vector3 startPos;
+
+	void Start() {
+		startPos = transform.position;
+	}
+	
+	void Update () {
+		transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+		Vector3 tempPos = startPos;
+		tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * floatFrequency)
+		             * floatAmplitude;
+		transform.position = tempPos;
+	}
+}
