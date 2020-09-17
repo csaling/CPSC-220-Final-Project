e8e4ae884d2a5357a427acb3b41509940f2d3307
Cade Saling <csaling@muskingum.edu>
2020-09-17 14:15:37 -0400

    Trying to implement script that detects mouse clicks

---
+++ Assets/Field Objects/POIMarker/OnTap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTap : MonoBehaviour
+{
+    private bool isLeftButtonDown = Input.GetMouseButton(0);
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (isLeftButtonDown)
+        {
+            Debug.Log("True or False?:" + isLeftButtonDown);
+        }
+    }
+}
